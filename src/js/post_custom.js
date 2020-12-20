eb801fb6ca1c1ec1a6af777a8487ad544277808c
sklose <mail@sklose.com>
2020-12-20 16:31:04 -0500

    automatically insert affiliate links for ingredients

--- src/js/post_custom.js
+++ src/js/post_custom.js
@@ -1,4 +1,5 @@
 import $ from 'jquery'
+import $redirects from './redirects.json'
 
 /**
 * Function that captures a click on an outbound link in Analytics.
@@ -15,28 +16,54 @@ var captureOutboundLink = function (url) {
     }
 }
 
+const $ingredients = $redirects.map((r) => {
+    const regex = new RegExp(r.regex, 'gmi')
+    return { regex, from: r.from }
+})
+
 $(document).ready(() => {
-    $("iframe").each(function () {
+    $('iframe').each(function () {        
         setTimeout(function(element) {
             const src = element.src
-            if (src.indexOf("youtube.com") >= 0) {
-                if (src.indexOf("?") > 0) {
-                    element.src = src + "&autoplay=1"
+            if (src.indexOf('youtube.com') >= 0) {
+                if (src.indexOf('?') > 0) {
+                    element.src = src + '&autoplay=1'
                 } else {
-                    element.src = src + "?autoplay=1"
+                    element.src = src + '?autoplay=1'
                 }
             }
         }, 2000, this)
     })
 
-    $("a").each(function() {
-        if (this.href.indexOf("/ingredients/") > 0) {
-            $(this).on("click", function() {
-                captureOutboundLink(this.href)
-                return false
-            })
-            this.target = "_blank"
-            $(this).wrap("<span class='icon-star' aria-hidden='true'></span>")
+    const heading = $('#ingredients')
+    heading.nextUntil(heading.attr('type')).find('li').each(function() {
+        const text = $(this).text()
+        for (const { regex, from } of $ingredients) {
+            const match = regex.exec(text)
+            if (match) {
+                const before = text.substr(0, regex.lastIndex - match[0].length)
+                const ingredient = text.substr(regex.lastIndex - match[0].length, match[0].length)
+                const after = text.substr(regex.lastIndex)
+                const anchor = $('<a></a>', {
+                    href: from,
+                    target: '_blank'
+                })
+                anchor.text(ingredient)
+                anchor.on('click', function() {
+                    captureOutboundLink(this.href)
+                    return false
+                })
+
+                const span = $('<span class="icon-star" aria-hidden="true"></span>')
+                span.append(anchor)
+
+                $(this).text('')
+                $(this).append(before)
+                $(this).append(span)
+                $(this).append(after)
+
+                break
+            }
         }
     })
 })
