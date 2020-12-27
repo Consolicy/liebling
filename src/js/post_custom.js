2c62fed2f323b05bde6ce7afe633bb9e171afeb5
sklose <mail@sklose.com>
2020-12-27 14:41:12 -0500

    remove custom tracking code

--- src/js/post_custom.js
+++ src/js/post_custom.js
@@ -1,21 +1,6 @@
 import $ from 'jquery'
 import $redirects from './redirects.json'
 
-/**
-* Function that captures a click on an outbound link in Analytics.
-* This function takes a valid URL string as an argument, and uses that URL string
-* as the event label. Setting the transport method to 'beacon' lets the hit be sent
-* using 'navigator.sendBeacon' in browser that support it.
-*/
-var captureOutboundLink = function (url) {
-    if (ga) {
-        ga('send', 'event', 'outbound', 'click', url, {
-            'transport': 'beacon',
-            'hitCallback': function () { document.location = url; }
-        });
-    }
-}
-
 const $ingredients = $redirects.map((r) => {
     const regex = new RegExp(r.regex, 'gmi')
     return { regex, from: r.from }
@@ -48,10 +33,6 @@ $(document).ready(() => {
                     target: '_blank'
                 })
                 anchor.text(ingredient)
-                anchor.on('click', function() {
-                    captureOutboundLink(this.href)
-                    return false
-                })
 
                 const span = $('<span class="icon-star" aria-hidden="true"></span>')
                 span.append(anchor)
